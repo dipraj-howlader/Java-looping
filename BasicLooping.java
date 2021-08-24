4a1a5f5f97c4d03337cb6f95615faaa1df4ae667
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-25 01:40:46 +0600

    looping

--- BasicLooping.java
+++ BasicLooping.java
@@ -2,11 +2,29 @@ package BasicLooping;
 
 public class BasicLooping {
 	public static void main(String[] args) {
+		//for loop
 		for(int i = 0 ; i<= 10; i++) {
 			System.out.println(i +" I love Bangladesh\n");
 		}
+		
+		//while loop
+		int y = 10;
+		while(y<=20) {
+			System.out.println(y +" I love Bangladesh\n");
+			y++;
+		}
+		
+		// do while
+		int z = 20;
+		
+		do {
+			System.out.println(z +" I love Bangladesh\n");
+			z++;
+		}
+		while(z<=30);
+		
 	}
 	
-	//for loop
+
 
 }
