947428f831087c86500b1730b6e75ffac6469b26
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-25 01:31:29 +0600

    new

---
+++ BasicLooping.java
@@ -0,0 +1,12 @@
+package BasicLooping;
+
+public class BasicLooping {
+	public static void main(String[] args) {
+		for(int i = 0 ; i<= 10; i++) {
+			System.out.println(i +" I love Bangladesh\n");
+		}
+	}
+	
+	//for loop
+
+}
